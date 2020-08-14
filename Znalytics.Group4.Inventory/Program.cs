d15b646b940b032a62b7b5c11b17f43cb6780ab3
Sushmago <sushmagoud0706@gmail.com>
2020-08-14 14:13:12 +0530

    new program is created

--- Znalytics.Group4.Inventory/Program.cs
+++ Znalytics.Group4.Inventory/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Znalytics.Group4.Inventory
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadKey();
         }
     }
 }
