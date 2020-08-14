6cea35c880d636219c7b3fe1d1d40e943a7e23aa
Sushmago <sushmagoud0706@gmail.com>
2020-08-14 14:48:17 +0530

    name

--- Znalytics.Group4.Inventory.BusinessLogicLayer/Class1.cs
+++ Znalytics.Group4.Inventory.BusinessLogicLayer/Class1.cs
@@ -1,12 +1,10 @@
-﻿
-
-namespace Znalytics.Group4.Inventory.BusinessLogicLayer
+﻿namespace Znalytics.Group4.Inventory.BusinessLogicLayer
 {
     public class Class1
     {
         public void Main()
         {
-            System.Console.WriteLine("Sush")
+            System.Console.WriteLine("Sush");
         }
     }
 }
