5b799a546f87071f6f2a54696a3659e0bddcfedd
Ramakrushal <ramakrushal99@gmail.com>
2020-08-14 13:14:07 +0530

    added name method in Class2.

---
+++ Znalytics.Group4.Inventory.DataAccessLayer/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znalytics.Group4.Inventory.DataAccessLayer
+{
+    class Class2
+    {
+        public void name() {
+            System.Console.WriteLine("hello");
+    }
+        
+    }
+}
