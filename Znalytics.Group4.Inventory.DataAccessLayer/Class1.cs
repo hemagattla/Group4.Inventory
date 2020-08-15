1dbce558e2cc172fcd4b23aca1621334ed48ee8f
Sushmago <sushmagoud0706@gmail.com>
2020-08-15 10:45:03 +0530

    rawmaterialclass

--- Znalytics.Group4.Inventory.DataAccessLayer/Class1.cs
+++ Znalytics.Group4.Inventory.DataAccessLayer/Class1.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Znalytics.Group4.Inventory.DataAccessLayer
-{
-    public class Class1
-    {
-    }
-}
+﻿
