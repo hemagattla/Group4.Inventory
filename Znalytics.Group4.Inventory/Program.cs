b2c5814d927615720ab7fc5a990a92b25d04ba7d
Ramakrushal <ramakrushal99@gmail.com>
2020-08-15 10:08:41 +0530

    aded product class and presentation part

--- Znalytics.Group4.Inventory/Program.cs
+++ Znalytics.Group4.Inventory/Program.cs
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-﻿
-
-
-
 ﻿class   A
-
 {
     static void Main()
-    { 
-       
-        
-            System.Console.WriteLine("Hello World");
-            System.Console.ReadKey();
-        }
-////ef5b56f5ac47728fa574887362bb31e2abc5e812
+    {
+        System.Console.WriteLine("ramya");
     }
-
+}
