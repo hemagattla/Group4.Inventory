2fe028e6127bfddbc0bef6541ec93f2600f55f46
nityasreecheera <nithyacheera98@gmail.com>
2020-08-19 16:08:53 +0530

    Added methods in IWareHouseBusinessLayer

--- Znalytics.Group4.Inventory.BusinessLogicLayer/IWareHouseBusinessLayer.cs
+++ Znalytics.Group4.Inventory.BusinessLogicLayer/IWareHouseBusinessLayer.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Znalytics.Inventory.WareHouseModule.Entities;
 
 namespace Znalytics.Group4.Inventory.BusinessLogicLayer
 {
    public interface IWareHouseBusinessLayer
     {
-        string getStockStatus();
-        int getStockQuantity();
-        
+        void AddWareHouse(WareHouse warehouse);
+         List<WareHouse> GetWareHouses();
+
+        WareHouse GetWareHouseByWareHouseID(string WareHouseID);
+
+
+
+
+
+
+        //string getStockStatus();
+        // int getStockQuantity();
+
     }
 }
