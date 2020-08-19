5d3ba4c08ed32c9da9fc5d3beabc0a6389fa5875
nityasreecheera <nithyacheera98@gmail.com>
2020-08-19 14:46:44 +0530

    Added a comment

--- Znalytics.Group4.Inventory.DataAccessLayer/WareHouseDataAcessLayer.cs
+++ Znalytics.Group4.Inventory.DataAccessLayer/WareHouseDataAcessLayer.cs
@@ -2,6 +2,9 @@
 
 using System.Collections.Generic;
 using Znalytics.Inventory.WareHouseModule.Entities;
+/// <summary>
+///     Represents the class for WareHouse Data
+/// </summary>
 public class WareHouseDataAccessLayer
 {
     //Created a list for WareHouse
