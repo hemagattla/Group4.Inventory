15142973731fba1a834b08bddaa680c222f11578
ramya <ramyasuram1999@gmail.com>
2020-08-19 07:17:44 -0700

    changed the reference in presentation

--- Znalytics.Group4.Inventory.DataAccessLayer/CustDataLayer.cs
+++ Znalytics.Group4.Inventory.DataAccessLayer/CustDataLayer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Znalytics.Group4.Inventory.DataAccessLayer
+namespace Znalytics.Group4.Inventory.DataAccessLayer 
 { 
         public class CustDataLayer
         {
