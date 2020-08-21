aaa167df80219e553a022c4854552da9e538c2f5
ramya <ramyasuram1999@gmail.com>
2020-08-21 01:47:06 -0700

    pushing changes

--- Znalytics.Group4.Inventory.Entities/Customer.cs
+++ Znalytics.Group4.Inventory.Entities/Customer.cs
@@ -23,7 +23,7 @@ public class Customer
     /// parameterized constructor(when ever an object is created  constructed is called)
     /// 
     /// </summary>
-   public Customer(int cid,string cstname,string mail,string mblno, string  cntry,string ci,string state,string st,string pin,string hno)
+  public Customer(int cid,string cstname,string mail,string mblno, string  cntry,string ci,string state,string st,string pin,string hno)
 
         {
         this.CustomerId = cid;///cuuurent method CoustomerId will be called and checks the conditons present in the method ant then asigns the value///
@@ -36,9 +36,9 @@ public class Customer
         this.Street = st;
         this.PinNo = pin;
         this.HNo = hno;
-
-
-        }
+    }/// <summary>
+    
+    /// </summary>
     public int CustomerId
     {
         set
@@ -176,10 +176,6 @@ public class Customer
             return _Street;
         }
     }
-
-
-
-
     public string PinNo
 
     {
@@ -189,7 +185,7 @@ public class Customer
             {
                 for (int i = 0; i < 6; i++)
                 {
-                    if (value[i] > 0 && value[i] < 9)
+                    if (value[i] > 0 || value[i] < 9)
                     {
                         _PinNo = value;
                     }
