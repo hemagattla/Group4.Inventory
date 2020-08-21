7fb1dc1e888eda8ae9f1dc79e5acdbf0b1cfce3a
Hema G <hema16g@outlook.com>
2020-08-21 12:58:18 +0530

    ......

--- Znalytics.Group4.Inventory.Entities/Stock.cs
+++ Znalytics.Group4.Inventory.Entities/Stock.cs
@@ -1,120 +1,126 @@
-﻿using System.Dynamic;
-/// <summary>
-/// Stock Details
-/// </summary>
-public class Stock
+﻿namespace Znalytics.Inventory.StockMaintain.Entities
 {
-    //Private fields
-    private string _WareHouseID;
-    private string _AddressName;
-    private string _ProductID;
-    private int _Quantity;
-
-    //Property of WareHouseID
-    public string WareHouseID
+    /// <summary>
+    /// Stock Details
+    /// </summary>
+    public class Stock
     {
-        set
-        {
-            //if input value and WareHouseID present in database matches then the value will be assigned to the _WareHouseID
-            //else it will throw an exception
-            if ("1234" == value)
-            {
-                _WareHouseID = value;
+        //Private fields
+        private string _WareHouseID;
+        private string _AddressName;
+        private string _ProductID;
+        private int _Quantity;
 
-            }
-            else
+        //Property of WareHouseID
+        public string WareHouseID
+        {
+            set
             {
-                throw new System.Exception("ur entered WareHouseID is invalid");
+                //if input value and WareHouseID present in database matches then the value will be assigned to the _WareHouseID
+                //else it will throw an exception
+                if ("1234" == value)
+                {
+                    _WareHouseID = value;
+
+                }
+                else
+                {
+                    throw new System.Exception("ur entered WareHouseID is invalid");
+                }
+            }
+
+            get
+            {
+                return _WareHouseID;
+            }
+        }
+        public string AddressName
+        {
+            set
+            {
+                //if input value and AddressID present in database matches then the value will be assigned to the _AddressID
+                //else it will throw an exception
+                if ("2345" == value)
+                {
+                    _AddressName = value;
+                }
+                else
+                {
+                    throw new System.Exception("ur entered AddressID is invalid");
+                }
+            }
+            get
+            {
+                return _AddressName;
             }
         }
 
-        get
+        public string ProductID
         {
-            return _WareHouseID;
-        }
-    }
-    public string AddressName
-    {
-        set
-        {
-            //if input value and AddressID present in database matches then the value will be assigned to the _AddressID
-            //else it will throw an exception
-            if ("2345" == value)
+            set
             {
-                _AddressName = value;
+                //if input value and ProductID present in database matches then the value will be assigned to the _ProductID
+                //else it will throw an exception
+                if ("3456" == value)
+                {
+                    _ProductID = value;
+                }
+                else
+                {
+                    throw new System.Exception("ur entered ProductID is invalid");
+                }
             }
-            else
+            get
             {
-                throw new System.Exception("ur entered AddressID is invalid");
+                return _ProductID;
             }
         }
-        get
-        {
-            return _AddressName;
-        }
-    }
 
-    public string ProductID
-    {
-        set
+        public int Add
         {
-            //if input value and ProductID present in database matches then the value will be assigned to the _ProductID
-            //else it will throw an exception
-            if ("3456"== value)
+            set
             {
-                _ProductID = value;
+                //it takes in put value and add to the exsisting quantity
+                _Quantity += value;
             }
-            else
+            get
             {
-                throw new System.Exception("ur entered ProductID is invalid");
+                return _Quantity;
             }
         }
-        get
+        public int Delete
         {
-            return _ProductID;
-        }
-    }
-
-    public int Add
-    {
-        set
-        {
-            //it takes in put value and add to the exsisting quantity
-            _Quantity += value;
-        }
-        get
-        {
-            return _Quantity;
-        }
-    }
-    public int Delete
-    {
-        set
-        {
-            //it will take input value and substract from the exsisting quantity
-            //after that if the quantity is more than 0 then only it will assign the value
-            //else it makes quantity =0 and returns
-            _Quantity -= value;
-            if (_Quantity > 0)
+            set
             {
+                //it will take input value and substract from the exsisting quantity
+                //after that if the quantity is more than 0 then only it will assign the value
+                //else it makes quantity =0 and returns
                 _Quantity -= value;
+                if (_Quantity > 0)
+                {
+                    _Quantity -= value;
+                }
+                else
+                {
+                    _Quantity = 0;
+                }
             }
-            else
+            get
             {
-                _Quantity = 0;
+                return _Quantity;
             }
         }
-        get
+        public int Quantity
         {
-            return _Quantity;
-        }
-    }
-    public int Quantity
-    {
-        get
-        {
-            return _Quantity;
-        }
-    }
+            set
+            {
 
+            }
+            get
+            {
+                return _Quantity;
+            }
+        }
+
+    }
 }
