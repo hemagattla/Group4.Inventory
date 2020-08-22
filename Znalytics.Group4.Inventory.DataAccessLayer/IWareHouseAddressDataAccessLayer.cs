181e022f0491d6fe212e95a340b654348a570cca
nityasreecheera <nithyacheera98@gmail.com>
2020-08-22 22:53:43 +0530

    Added interface in DAL

---
+++ Znalytics.Group4.Inventory.DataAccessLayer/IWareHouseAddressDataAccessLayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+//Created by Nitya
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Znalytics.Inventory.AddressModule.Entities;
+
+namespace Znalytics.Inventory.AddressModule.DataAccessLayer
+{
+    interface IWareHouseAddressDataAccessLayer
+    {
+        void AddAddress(WareHouseAddress a);
+        List<WareHouseAddress> GetAddresses();
+        WareHouseAddress GetAddressByAddressID(string AddressID);
+        WareHouseAddress GetAddressByWareHouseID(string WareHouseID);
+    }
+}
