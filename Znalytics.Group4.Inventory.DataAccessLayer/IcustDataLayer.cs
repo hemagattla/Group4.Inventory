c6d384993081121499e41e5c7c3fd6534888b842
ramya <ramyasuram1999@gmail.com>
2020-08-23 00:57:24 -0700

    cahnges in interface

--- Znalytics.Group4.Inventory.DataAccessLayer/IcustDataLayer.cs
+++ Znalytics.Group4.Inventory.DataAccessLayer/IcustDataLayer.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Znalytics.Group4.Inventory.Entities;
-namespace Znalytics.Group4.Inventory.DataAccessLayer
+using Znalytics.Inventory.Module.Entities;
+namespace Znalytics.Inventory.IDataAccessLayer
 {
     class IcustDataLayer
     {
-       abstract  void AddCustomer(Customer c);
-        abstract void UpDateCustomer(Customer c);
-        abstract void DelCustomer(Customer c);
+       abstract public void AddCustomer(Customer c);
+        abstract public  void UpDateCustomer(Customer c);
+        abstract public void DelCustomer(Customer c);
     }
 }
