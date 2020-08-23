15f73aa3e9134d457033da7c47c68b1924994137
Ramakrushal <ramakrushal99@gmail.com>
2020-08-23 15:18:40 +0530

    made chaches of namespace

--- Znalytics.Group4.Inventory.BusinessLogicLayer/IProductBusinessLogicLayer.cs
+++ Znalytics.Group4.Inventory.BusinessLogicLayer/IProductBusinessLogicLayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Znalytics.Inventory.Product.Entities;
 
-namespace Znalytics.Group4.Inventory.BusinessLogicLayer
+namespace Znalytics.Group4.Inventory.IBusinessLogicLayer
 {
     public interface IProductBusinessLogicLayer
     {
