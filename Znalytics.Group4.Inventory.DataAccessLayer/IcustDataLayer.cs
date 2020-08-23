230e336f1fbf55b0a9ba0cbd51c0530428bb600a
ramya <ramyasuram1999@gmail.com>
2020-08-22 21:51:20 -0700

    icustdata layer

---
+++ Znalytics.Group4.Inventory.DataAccessLayer/IcustDataLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Znalytics.Group4.Inventory.Entities;
+namespace Znalytics.Group4.Inventory.DataAccessLayer
+{
+    class IcustDataLayer
+    {
+       abstract  void AddCustomer(Customer c);
+        abstract void UpDateCustomer(Customer c);
+        abstract void DelCustomer(Customer c);
+    }
+}
