878e2fffe9ac26560a143c80e2ba6614fd5685e2
ramya <ramyasuram1999@gmail.com>
2020-08-22 19:46:40 -0700

    added interface in data access

---
+++ Znalytics.Group4.Inventory.DataAccessLayer/ICustDataAccessLogic.c
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znalytics.Group4.Inventory.DataAccessLayer
+{
+    class ICustDataAccessLogic//creating an interface 
+    {
+    abstract void AddCustomer(Customer c);
+    abstract void DelCustomer(Customer c);
+    abstract void UpDateCustomer(Customer c);
+    }
+}
