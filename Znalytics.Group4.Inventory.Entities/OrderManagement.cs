e2ba85c992a9303f9c81757df47ee82ed085a477
Dhanasri-123 <dhanasrithanugula@gmail.com>
2020-08-24 17:37:17 +0530

    new

---
+++ Znalytics.Group4.Inventory.Entities/OrderManagement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Znalytics.Group4.Inventory.Entities
+{
+    class OrderManagement
+    {
+        List<string> _productName;
+        WareHouseAddress _wareHouseAddress;
+        CustomerAddress _customerAddress;
+        double _quantity;
+        double _price;
+        OrderManagement(List<string> productName, WareHouseAddress wareHouseAddress,CustomerAddress customerAddress,double quantity,double price)
+        {
+            productName = _productName;
+            wareHouseAddress = _wareHouseAddress;
+            customerAddress = _customerAddress;
+            quantity = _quantity;
+            price = _price;
+        }
+       Public List<string> ProductName{
+            set{
+                _productName = value;
+            }
+            get{
+                return _productName;
+            }
+        }
+        public WareHouseAddress WareHouseAddress
+        {
+            set
+            {
+                _wareHouseAddress = value;
+            }
+            get
+            {
+                return _wareHouseAddress;
+            }
+        } 
+        public CustomerAddress CustomerAddress
+        {
+            set
+            {
+                _customerAddress = value;
+            }
+            get
+            {
+                return _customerAddress;
+            }
+        }
+        public double Quantity
+        {
+            set
+            {
+                _quantity = value;
+            }
+            get
+            {
+                return _quantity;
+            }
+        }
+        public double Price
+        {
+            set
+            {
+                _price = value;
+            }
+            get
+            {
+                return _price;
+            }
+        }
+
+     
+        
+    } 
+}
