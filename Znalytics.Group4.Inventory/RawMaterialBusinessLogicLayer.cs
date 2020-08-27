adeb1f339819e3e18e8b1c7facfab22dea2f4935
Hema G <hema16g@outlook.com>
2020-08-27 17:26:32 +0530

    .....

---
+++ Znalytics.Group4.Inventory/RawMaterialBusinessLogicLayer.cs
@@ -0,0 +1,9 @@
+﻿namespace Znalytics.Group4.Inventory.RawMaterial.PresentationLayer
+{
+    internal class RawMaterialBusinessLogicLayer
+    {
+        public RawMaterialBusinessLogicLayer()
+        {
+        }
+    }
+}
