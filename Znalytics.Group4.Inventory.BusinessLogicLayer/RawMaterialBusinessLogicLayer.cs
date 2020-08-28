1aed1009fafb24bba7c081749f210ac79ac46a61
Hema G <hema16g@outlook.com>
2020-08-29 00:04:58 +0530

    changes done

--- Znalytics.Group4.Inventory.BusinessLogicLayer/RawMaterialBusinessLogicLayer.cs
+++ Znalytics.Group4.Inventory.BusinessLogicLayer/RawMaterialBusinessLogicLayer.cs
@@ -1,45 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Znalytics.Group4.Inventory.RawMaterialModule.Entities;
 using Znalytics.Group4.Inventory.RawMaterialModule.DataAccessLayer;
-
+using Znalytics.Group4.Inventory.IRawMaterialModule.BusinessLogicLayer;
+using Znalytics.Group4.Inventory.RawMaterialModule.EntityLayer;
 
 namespace Znalytics.Group4.Inventory.RawMaterialModule.BusinessLogicLayer
 {
-    public  class RawMaterialBusinessLogicLayer
+    /// <summary>
+    ///Public class RawMaterialBusinessLogicLayer 
+    /// </summary>
+    public class RawMaterialBusinessLogicLayer : IRawMaterialBusinessLogicLayer//Interface Name
     {
+        //creating reference variable of RawMaterialDataAccessLayer class
         RawMaterialDataAccessLayer _rawMaterialDataAccessLayer;
-       public RawMaterialBusinessLogicLayer()
+        //public Constructor
+        public RawMaterialBusinessLogicLayer()
         {
-            _rawMaterialDataAccessLayer = new RawMaterialDataAccessLayer();
+            _rawMaterialDataAccessLayer = new RawMaterialDataAccessLayer();//creating object of RawMaterialDataAccessLayer class
         }
+        //AddRawMaterial Method
         public void AddRawMaterial(RawMaterial rawMaterial)
         {
-            if ((rawMaterial.RawMaterialName != null)&& (rawMaterial.RawMaterialID!=null)&&(rawMaterial.Quantity!=null)&&(rawMaterial.Units!=null)&&(rawMaterial.Price!=0D))
+            try
             {
-                _rawMaterialDataAccessLayer.AddRawMaterial(rawMaterial);
+                if ((rawMaterial.RawMaterialName != null) && (rawMaterial.RawMaterialID != null) && (rawMaterial.Quantity != 0D) && (rawMaterial.Units != null) && (rawMaterial.Price != 0D))
+                {
+                    _rawMaterialDataAccessLayer.AddRawMaterial(rawMaterial);
+                }
+                else
+                {
+                    throw new RawMaterialException("ur entered input not valid");
+                }
+            }
+            catch
+            {
+                throw;
             }
         }
+        //Deletes the RawMaterial details
         public void DeleteRawMaterial(RawMaterial rawMaterial)
         {
-            if ((rawMaterial.RawMaterialName != null) && (rawMaterial.RawMaterialID != null) && (rawMaterial.Quantity != null) && (rawMaterial.Units != null) && (rawMaterial.Price != 0D))
+            try
             {
-                _rawMaterialDataAccessLayer.DeleteRawMaterial(rawMaterial);
+                if ((rawMaterial.RawMaterialName != null) && (rawMaterial.RawMaterialID != null) && (rawMaterial.Quantity != 0D) && (rawMaterial.Units != null) && (rawMaterial.Price != 0D))
+                {
+                    _rawMaterialDataAccessLayer.DeleteRawMaterial(rawMaterial);
+                }
+                else
+                {
+                    throw new RawMaterialException("ur entered input not valid");
+                }
+            }
+            catch
+            {
+                throw;
             }
         }
+        //returns the list of RawMaterial details
         public List<RawMaterial> GetRawMaterial()
         {
+
             return _rawMaterialDataAccessLayer.GetRawMaterial();
+
         }
-        public void UpdateRawMaterial(RawMaterial rawMaterial)
+        //used to update price of the RawMaterial
+        public void UpdateRawMaterialPrice(RawMaterial rawMaterial)
         {
-            if((rawMaterial.Price!=0D)||(rawMaterial.Quantity!=null))
+            try
             {
-                _rawMaterialDataAccessLayer.UpdateRawMaterial(rawMaterial);
+                if (rawMaterial.Price != 0D)
+                {
+                    _rawMaterialDataAccessLayer.UpdateRawMaterialPrice(rawMaterial);
+                }
+                else
+                {
+                    throw new RawMaterialException("ur entered input not valid");
+                }
             }
+            catch
+            {
+                throw;
+            }
+
+        }
+        //Updates the Quantity of RawMaterial
+        public void UpdateRawMaterialQuantity(RawMaterial rawMaterial)
+        {
+
+            try
+            {
+                if (rawMaterial.Quantity != 0D)
+                {
+                    _rawMaterialDataAccessLayer.UpdateRawMaterialQuantity(rawMaterial);
+                }
+                else
+                {
+                    throw new RawMaterialException("ur entered input not valid");
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        //represents whethere the RawMaterial exists or not based on RawMaterialID
+        public RawMaterial GetRawMaterialByRawMaterialID(string RawMaterialID)
+        {
+            return _rawMaterialDataAccessLayer.GetRawMaterialByRawMaterialID(RawMaterialID);
+
         }
     }
 }
---
+++ Znalytics.Group4.Inventory.Entities/RawMaterialException.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znalytics.Group4.Inventory.RawMaterialModule.EntityLayer
+
+{
+    public class RawMaterialException : ApplicationException
+    {
+
+        public RawMaterialException() : base()
+        {
+
+        }
+
+        public RawMaterialException(string message) : base(message)
+        {
+
+        }
+
+
+
+    }
+}
