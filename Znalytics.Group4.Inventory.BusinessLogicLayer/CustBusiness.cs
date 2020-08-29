a212e38c1e935223cda6f200ada733fded04240b
Hema G <hema16g@outlook.com>
2020-08-29 16:02:24 +0530

    ....................

--- Znalytics.Group4.Inventory.BusinessLogicLayer/CustBusiness.cs
+++ Znalytics.Group4.Inventory.BusinessLogicLayer/CustBusiness.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -48,5 +48,5 @@ namespace Znalytics.Inventory.Module.BusinessLogicLayer///namespace is nothing b
         }
 
     }
-}
+}*/
 
