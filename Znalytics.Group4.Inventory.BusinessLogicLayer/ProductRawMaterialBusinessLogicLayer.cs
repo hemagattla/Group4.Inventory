fc3fb7dd240ebfd62035945e10569ae5c7a71aab
Hema G <hema16g@outlook.com>
2020-08-30 22:49:54 +0530

    ....

--- Znalytics.Group4.Inventory.BusinessLogicLayer/ProductRawMaterialBusinessLogicLayer.cs
+++ Znalytics.Group4.Inventory.BusinessLogicLayer/ProductRawMaterialBusinessLogicLayer.cs
@@ -5,28 +5,33 @@ using Znalytics.Group4.Inventory.ProductModule.IBusinessLogicLayer;
 using Znalytics.Group4.Inventory.IRawMaterialModule.BusinessLogicLayer;
 using Znalytics.Group4.Inventory.ProductRawMaterialModule.Entities;
 using Znalytics.Group4.Inventory.RawMaterialModule.Entities;
-using Znalytics.Group4.Inventory.RawMaterialModule.DataAccessLayer;
+//using Znalytics.Group4.Inventory.RawMaterialModule.DataAccessLayer;
 
 using Znalytics.Inventory.ProductModule.Entitie;
-using Znalytics.Inventory.ProductModule.DataAccessLayer;
+//using Znalytics.Inventory.ProductModule.DataAccessLayer;
 using Znalytics.Group4.Inventory.ProductRawMaterialModule.DataAccessLayer;
+
+using Znalytics.Group4.Inventory.RawMaterialModule.BusinessLogicLayer;
+using Znalytics.Inventory.ProductModule.BusinessLogicLayer;
 
 namespace Znalytics.Group4.Inventory.ProductRawMaterialModule.BusinessLogicLayer
 {
-    public class ProductRawMaterialBusinessLogicLayer : IProductBusinessLogicLayer, IRawMaterialBusinessLogicLayer
+    public class ProductRawMaterialBusinessLogicLayer
     {
+        IRawMaterialBusinessLogicLayer irm = new RawMaterialBusinessLogicLayer();
+        IProductBusinessLogicLayer ipb = new ProductBusiness();
         //ProductBusiness pb = new ProductBusiness();
         //RawMaterialBusinessLogicLayer rawMaterialBusinessLogicLayer = new RawMaterialBusinessLogicLayer();
         ProductRawMaterialDataAccessLayer prda;
-        RawMaterialDataAccessLayer rawMaterialDataAccessLayer = new RawMaterialDataAccessLayer();
-        ProductDataAccessLogic productData = new ProductDataAccessLogic();
+        //RawMaterialDataAccessLayer rawMaterialDataAccessLayer = new RawMaterialDataAccessLayer();
+        //ProductData productData = new ProductData();
         public ProductRawMaterialBusinessLogicLayer()
         {
-            prda = new ProductRawMaterialDataAccessLayer();
+              prda = new ProductRawMaterialDataAccessLayer();
         }
         public void AddRawMaterialToProduct(ProductRawMaterial productRawMaterial)
         {
-            if ((productRawMaterial.ProductID != null) && (productRawMaterial.RawMaterialID != null) && (productRawMaterial.RawMaterialName != null) && (productRawMaterial.Quantity != 0D) && (productRawMaterial.Units != null))
+            if((productRawMaterial.ProductID!=null)&&(productRawMaterial.RawMaterialID != null)&&(productRawMaterial.RawMaterialName != null)&&(productRawMaterial.Quantity != 0D)&&(productRawMaterial.Units != null))
             {
                 prda.AddRawMaterialToProduct(productRawMaterial);
             }
@@ -61,14 +66,32 @@ namespace Znalytics.Group4.Inventory.ProductRawMaterialModule.BusinessLogicLayer
                 System.Console.WriteLine("Entered Values contail null values");
             }
         }
-        public Product GetProductByProductID(string ProductID) { return productData.GetProductByID(ProductID); }
+        public Product GetProductByProductID(string ProductID)
+        {
+            return ipb.GetProductByProductID(ProductID);
+        }
         public RawMaterial GetRawMaterialByRawMaterialID(string RawMaterialID)
+        {
+            return irm.GetRawMaterialByRawMaterialID(RawMaterialID);
+        }
+
+            //public Product GetProductByProductID(string ProductID) { return productData.GetProductByID(ProductID); }
+           /* public RawMaterial GetRawMaterialByRawMaterialID(string RawMaterialID)
         {
             return rawMaterialDataAccessLayer.GetRawMaterialByRawMaterialID(RawMaterialID);
 
+        }*/
+        public List<ProductRawMaterial> GetDetailsByProductID(ProductRawMaterial productRawMaterial)
+        {
+            return prda.GetDetailsByProductID(productRawMaterial);
         }
+        public List<ProductRawMaterial> GetDetailsUsingOriderBy()
+        {
+            return prda.GetProductRawMaterial();
+        }
+        
 
-        public void AddProducts(Product n)
+            public void AddProducts(Product n)
         {
             throw new NotImplementedException();
         }
@@ -99,11 +122,6 @@ namespace Znalytics.Group4.Inventory.ProductRawMaterialModule.BusinessLogicLayer
         }
 
         public void RemoveProduct(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<ProductRawMaterial> GetDetailsByProductID(ProductRawMaterial productRawMaterial)
         {
             throw new NotImplementedException();
         }
